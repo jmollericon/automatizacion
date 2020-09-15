ba390cda26706ca2fa12c6f7d4a34f3c1995b12f
Jorge Mollericon <jlmollericon@gmail.com>
2020-09-15 11:45:37 -0400

    add: chomedriver, example.

--- Class4/src/test/java/base/BaseTest.java
+++ Class4/src/test/java/base/BaseTest.java
@@ -1,4 +1,22 @@
 package base;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class BaseTest {
+    private WebDriver driver;
+
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "resources/chromedriver");
+        driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/");
+        System.out.println(driver.getTitle());
+        driver.manage().window().maximize();
+    }
+
+    public static void main(String[] args) {
+        BaseTest baseTest = new BaseTest();
+        baseTest.setUp();
+    }
+
 }
