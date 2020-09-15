a399f9e4781c4e698efc49976e023ebfa545ab01
Jorge Mollericon <jlmollericon@gmail.com>
2020-09-15 12:00:33 -0400

    multiple elements and close browser

--- Class4/src/test/java/base/BaseTest.java
+++ Class4/src/test/java/base/BaseTest.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.List;
+
 public class BaseTest {
     private WebDriver webDriver;
 
@@ -12,14 +14,19 @@ public class BaseTest {
         System.setProperty("webdriver.chrome.driver", "resources/chromedriver");
         webDriver = new ChromeDriver();
         webDriver.get("https://the-internet.herokuapp.com/");
-        System.out.println(webDriver.getTitle());
+        System.out.println(webDriver.getTitle()); // Titulo de la página
         webDriver.manage().window().maximize();
+
+        List<WebElement> elementList = webDriver.findElements(By.tagName("a"));
+        System.out.println("Cantidad de links: " + elementList.size());
 
         WebElement element = webDriver.findElement(By.linkText("Inputs"));
         element.click();
 
         WebElement inputElement = webDriver.findElement(By.tagName("input"));
         inputElement.sendKeys("12345678");
+
+        webDriver.quit(); // Cerrar navegador
     }
 
     public static void main(String[] args) {
