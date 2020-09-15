0a00e65816d770b3d189eb0b922d468efa897452
Jorge Mollericon <jlmollericon@gmail.com>
2020-09-15 11:50:27 -0400

    Basic example, selenium chrome driver.

--- Class4/src/test/java/base/BaseTest.java
+++ Class4/src/test/java/base/BaseTest.java
@@ -1,17 +1,25 @@
 package base;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BaseTest {
-    private WebDriver driver;
+    private WebDriver webDriver;
 
     public void setUp() {
         System.setProperty("webdriver.chrome.driver", "resources/chromedriver");
-        driver = new ChromeDriver();
-        driver.get("https://the-internet.herokuapp.com/");
-        System.out.println(driver.getTitle());
-        driver.manage().window().maximize();
+        webDriver = new ChromeDriver();
+        webDriver.get("https://the-internet.herokuapp.com/");
+        System.out.println(webDriver.getTitle());
+        webDriver.manage().window().maximize();
+
+        WebElement element = webDriver.findElement(By.linkText("Inputs"));
+        element.click();
+
+        WebElement inputElement = webDriver.findElement(By.tagName("input"));
+        inputElement.sendKeys("12345678");
     }
 
     public static void main(String[] args) {
