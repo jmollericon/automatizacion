5c6c19708018b2f79c0e7eec5383efae2f31bd13
Jorge Mollericon <jlmollericon@gmail.com>
2020-09-16 17:55:16 -0400

    successfulLogin with extentreportes

---
+++ Class8_Login_Report/src/test/java/base/BaseTest.java
@@ -0,0 +1,65 @@
+package base;
+
+import com.aventstack.extentreports.Status;
+import helper.ScreenShotHelper;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import report.ReportManager;
+
+public abstract class BaseTest {
+    protected WebDriver webDriver;
+
+    @BeforeSuite
+    public static void setUpSuite() throws Exception {
+        ReportManager.init("/home/jorge/Documents/reports", "LoginSite");
+    }
+
+    @BeforeMethod
+    public void setUp(ITestResult iTestResult){
+        ReportManager.getInstance().startTest(iTestResult.getMethod().getMethodName());
+
+        System.setProperty("webdriver.chrome.driver","resources/chromedriver");
+        webDriver = new ChromeDriver();
+        webDriver.get("https://testfaceclub.com/ejercicios/");
+    }
+
+    @AfterMethod
+    public void tearDown(ITestResult iTestResult){
+        try {
+            switch (iTestResult.getStatus()){
+                case ITestResult.FAILURE:
+                    ReportManager.getInstance().getTest().log(Status.FAIL, "Test failed");
+                    break;
+                case ITestResult.SKIP:
+                    ReportManager.getInstance().getTest().log(Status.SKIP, "Test skipped");
+                    break;
+                case ITestResult.SUCCESS:
+                    ReportManager.getInstance().getTest().log(Status.PASS, "Test passed");
+                    break;
+                default:
+                    ReportManager.getInstance().getTest().log(Status.FAIL, "Test incomplete");
+            }
+
+            if(iTestResult.getStatus() != ITestResult.SUCCESS && iTestResult.getThrowable() != null){
+                ReportManager.getInstance().getTest().log(Status.FAIL, iTestResult.getThrowable().getMessage());
+                ScreenShotHelper.takeScreenShotAndAdToHTMLReport(webDriver, Status.FAIL, "Failure Image");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            if(webDriver != null)
+                webDriver.quit();
+        }
+    }
+
+    @AfterSuite
+    public static void tearDownSuite(){
+        ReportManager.getInstance().flush();
+    }
+}
